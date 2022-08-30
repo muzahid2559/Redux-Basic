9cc52295c7f742ca6cef98ff80b215445c0eb38c
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-31 02:37:16 +0600

    Dispatching Actions

--- index.js
+++ index.js
@@ -9,9 +9,35 @@ const initState = {
 
 //Reducer
 const rootReducer = (state = initState, action) =>{
+    if(action.type === 'INC_NUM'){
+        return{
+            ...state,
+            num: state.num+1
+        }
+    }
+
+    if(action.type === 'ADD_NUM'){
+        return{
+            ...state,
+            num: state.num + action.ChangeValue
+        }
+    }
+
     return state;
 }
 
 // Store
 const store = redux.createStore(rootReducer);
+console.log(store.getState());
+
+// Dispatching Action
+store.dispatch({
+    type : 'INC_NUM',
+});
+
+store.dispatch({
+    type : 'ADD_NUM',
+    ChangeValue : 34,
+});
+
 console.log(store.getState());
